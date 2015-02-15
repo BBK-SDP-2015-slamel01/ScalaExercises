38c58d582554fdf50f8bbf748920d645c8787483
Sergio <rurounisergii@hotmail.com>
2015-02-15 00:19:40 +0000

    Doing lesson 1 Method exercises

---
+++ ScalaExercises/src/exercises/Exercise1.scala
@@ -0,0 +1,17 @@
+package exercises
+import scala.math._
+
+class Exercise1 {
+  def getSquare(num: Int) = { //don't forget = sign
+    sqrt(num)
+  }
+  def isArg1GreaterThanArg2(num1: Double, num2: Double) = {
+    num1 > num2
+   // if (num1 > num2) true
+   // else false
+  }
+  def manyTimesString(str: String, num: Int) = {
+    str * num
+  }
+
+}
